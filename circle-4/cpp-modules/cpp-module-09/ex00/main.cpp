cac97e303640e4a030dfbbbfe3ed9f046c85b764
belmoussaoui <emb-12@outlook.com>
2023-05-03 20:27:33 +0200

    start check valid date

--- circle-4/cpp-modules/cpp-module-09/ex00/main.cpp
+++ circle-4/cpp-modules/cpp-module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bel-mous <bel-mous@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 02:22:08 by bel-mous          #+#    #+#             */
-/*   Updated: 2023/05/03 19:23:18 by bel-mous         ###   ########.fr       */
+/*   Updated: 2023/05/03 20:21:45 by bel-mous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <string>
 #include <fstream>
 #include <sys/stat.h>
+#include <sstream>
 
 bool is_file(const char *path)
 {
@@ -34,14 +35,88 @@ bool check_file_argument(int argc, char *path)
 	if (!inputFile.is_open() || !is_file(path))
 	{
 		std::cout << "The argument " << path << " must be a file." << std::endl;
-		return 1;
+		return true;
 	}
-	return (true);
+	return true;
+}
+
+bool is_valid_date(const std::string &date)
+{
+	// Vérifie que la date est au format "Year-Month-Day"
+	int year, month, day;
+	std::stringstream ss(date);
+
+	ss >> year;
+	if (ss.fail())
+	{
+		std::cerr << "Failed to extract year from date string" << std::endl;
+		return false;
+	}
+	ss.ignore(1); // Ignore the first separator
+	ss >> month;
+	if (ss.fail())
+	{
+		std::cerr << "Failed to extract month from date string" << std::endl;
+		return false;
+	}
+	ss.ignore(1); // Ignore the second separator
+	ss >> day;
+	if (ss.fail())
+	{
+		std::cerr << "Failed to extract day from date string" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool is_valid_value(const std::string &value)
+{
+	// Vérifie que la valeur est comprise entre 0 et 1000
+	std::stringstream ss(value);
+	float val;
+	if (ss >> val && val >= 0 && val <= 1000 && ss.eof())
+	{
+		return true;
+	}
+	return false;
+}
+
+bool check_input_file(char *path)
+{
+	std::ifstream file(path);
+
+	std::string line;
+	std::getline(file, line);
+	while (std::getline(file, line))
+	{
+		std::stringstream ss(line);
+		std::string date, value;
+		if (std::getline(ss, date, '|') && std::getline(ss, value))
+		{
+			if (!is_valid_date(date) || !is_valid_value(value))
+			{
+				std::cerr << "valid_date = " << is_valid_date(date) << " valid_value = " << is_valid_value(value) << std::endl;
+				std::cerr << "Invalid format: " << line << "\n";
+			}
+			else
+			{
+				std::cerr << "Valid format: " << line << "\n";
+			}
+		}
+		else
+		{
+			std::cerr << "Invalid format: " << line << "\n";
+		}
+		std::cerr << "\n";
+	}
+	return true;
 }
 
 int main(int argc, char *argv[])
 {
 	if (check_file_argument(argc, argv[1]))
-		;
+	{
+		check_input_file(argv[1]);
+	}
 	return EXIT_SUCCESS;
 }
