08d9c52dab16a9dc2b26733e9d27e439ab14e7d2
belmoussaoui <emb-12@outlook.com>
2023-05-15 18:53:37 +0200

    insertion sort

--- circle-4/cpp-modules/cpp-module-09/ex02/PmergeMe.cpp
+++ circle-4/cpp-modules/cpp-module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: bel-mous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 13:30:49 by bel-mous          #+#    #+#             */
-/*   Updated: 2023/05/15 15:39:23 by bel-mous         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:52:53 by bel-mous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,12 +78,55 @@ void PmergeMe::checkSequence(std::vector<std::string> sequence)
 	}
 }
 
+template <typename Container>
+void PmergeMe::insertionSort(Container &container)
+{
+	typename Container::iterator it, j;
+
+	for (it = container.begin(); it != container.end(); ++it)
+	{
+		typename Container::value_type key = *it;
+		j = it;
+		while (j != container.begin() && *(std::prev(j)) > key)
+		{
+			*j = *(std::prev(j));
+			--j;
+		}
+		*j = key;
+	}
+}
+
+template <typename Container>
+void PmergeMe::mergeSort(Container &container)
+{
+	(void)container;
+}
+
+template <typename Container>
+double PmergeMe::mergeInsertSort(Container &container)
+{
+	clock_t startTime = clock();
+	insertionSort(container);
+ 	clock_t endTime = clock();
+	double time = static_cast<double>(endTime - startTime) / CLOCKS_PER_SEC * 1000;
+	return time;
+}
+
 void PmergeMe::run(std::vector<std::string> sequence)
 {
 	container1.clear();
 	container2.clear();
 	checkSequence(sequence);
-	// double time1 = runFirstContainer()
-	// double time2 = runSecondContainer();
-	// displaySequence(time1, time2);
+	double time1 = mergeInsertSort(container1);
+	double time2 = mergeInsertSort(container2);
+	displaySequence(sequence, time1, time2);
+}
+
+void PmergeMe::displaySequence(std::vector<std::string> sequence, double time1, double time2)
+{
+	(void) sequence;
+	std::cout.setf(std::ios::fixed);
+    std::cout.precision(5);
+	std::cout << "Time to process a range of 3000 elements with std::[..] : " << time1 << "us" << std::endl;
+	std::cout << "Time to process a range of 3000 elements with std::[..] : " << time2 << "us" << std::endl;
 }
--- circle-4/cpp-modules/cpp-module-09/ex02/PmergeMe.hpp
+++ circle-4/cpp-modules/cpp-module-09/ex02/PmergeMe.hpp
@@ -6,10 +6,9 @@
 /*   By: bel-mous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 13:36:47 by bel-mous          #+#    #+#             */
-/*   Updated: 2023/05/15 15:31:40 by bel-mous         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:49:16 by bel-mous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef PMERGE_HPP
 #define PMERGE_HPP
@@ -18,20 +17,31 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <ctime>
+
 
 class PmergeMe
 {
 private:
 	std::vector<int> container1;
 	std::list<int> container2;
+
+	void checkSequence(std::vector<std::string> sequence);
+	void checkIsDigit(std::string number);
+	template <typename Container>
+	double mergeInsertSort(Container &container);
+	template <typename Container>
+	void insertionSort(Container &container);
+	template <typename Container>
+	void mergeSort(Container &container);
+	void displaySequence(std::vector<std::string> sequence, double time1, double time2);
+
 public:
 	PmergeMe();
 	~PmergeMe();
 	PmergeMe(PmergeMe const &other);
-	PmergeMe& operator=(PmergeMe const &other);
+	PmergeMe &operator=(PmergeMe const &other);
 	void run(std::vector<std::string> sequence);
-	void checkSequence(std::vector<std::string> sequence);
-	void checkIsDigit(std::string number);
 };
 
 #endif
